1bc121693563a2a1cdd4c7ab87b4de1d2725058f
Jolls <jollyint@gmail.com>
2015-12-23 13:04:18 -0600

    Script revised to add code as per assignment 2 requirements: cache inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,55 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation 
+##     and there may be some benefit to caching the inverse 
+##     of a matrix rather than computing it repeatedly
 
-## Write a short comment describing this function
+## These 2 functions can be use to cache the inverse of a matrix
+
+## The 1st function creates the matrix object and caches its inverse
+## It returns a list of functions used by cacheSolve function to
+##    to return the inverse of matrix:
+##      1. set the matrix
+##      2. get the matrix
+##      3. set inverse of the matrix
+##      4. get inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        cacheX <- NULL
+        set    <- function(y) {
+                                x      <<- y
+                                cacheX <<- NULL
+        }
+        
+        get        <- function() x
+        setinverse <- function(inverse) cacheX <<- inverse
+        getinverse <- function() cacheX
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## The 2nd function checks returns the inverse of the matrix 'x'
+## If the inverse of the matrix is cached it returns the cached information
+## If the inverse of the matrix is not cached it will calculate the inverse
+##    of the matrix, set it in cache and return the information
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        cacheX <- x$getinverse()
+        
+        ## Getting cached data if it exists
+        if(!is.null(cacheX)) {
+                print("Getting cached data.")
+                return(cacheX)
+        }
+        ## Getting cached data if it does not exist
+        data <- x$get()
+        cacheX <- solve(data, ...)
+        
+        ## cached data
+        x$setinverse(cacheX)
+        print("Caching data")
+        
+        ## return cached data
+        cacheX
 }
